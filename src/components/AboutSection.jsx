41645cc4d16ce276417a50a49f12bebb50ebce3b
Yefry Sanchez <yefrymsp@gmail.com>
2024-05-17 20:31:17 -0400

    about animation

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -3,20 +3,37 @@ import { Link } from "react-router-dom";
 import { fadeUp } from "../animations/animation";
 
 const AboutSection = () => {
-  
   return (
-    <div  className="px-4 text-shade1 lg:h-[1000px] 2xl:h-screen mb-12 flex flex-col items-center justify-center">
+    <div className="px-4 text-shade1 lg:h-[1000px] 2xl:h-screen mb-12 flex flex-col items-center justify-center">
       <div className="">
-        <motion.p variants={fadeUp} initial="initial" viewport={{once:true}} whileInView={"animate"} className="text-2xl md:text-3xl lg:text-5xl xl:text-7xl font-medium tracking-tight lg:max-w-[1100px] xl:max-w-[1400px] mx-auto">
-          I'm a Web Developer with a passion for Front-End Development.
-          I build visually stunning websites that leave a deep
-          and lasting impression on users. Explore my portfolio and see how I
-          can bring your vision to life!
+        <motion.p
+          variants={fadeUp}
+          initial="initial"
+          viewport={{ once: true }}
+          whileInView={"animate"}
+          className="text-2xl md:text-3xl lg:text-5xl xl:text-7xl font-medium tracking-tight lg:max-w-[1100px] xl:max-w-[1400px] mx-auto"
+        >
+          I'm a Web Developer with a passion for Front-End Development. I build
+          visually stunning websites that leave a deep and lasting impression on
+          users. Explore my portfolio and see how I can bring your vision to
+          life!
         </motion.p>
       </div>
-      < Link to={"/about"} variants={fadeUp} initial="initial" viewport={{once:true}} whileInView={"animate"} className="w-40 h-40 bg-shade2/15 hover:bg-shade1 hover:text-shade3 transition duration-300 rounded-full flex justify-center items-center ml-auto mr-8 mt-8 lg:ml-0 lg:mr-0 lg:mt-24">
-        <span className="text-xl font-medium">About Me</span>
-      </Link>
+      <motion.div variants={fadeUp}
+          initial="initial"
+          viewport={{ once: true }}
+          whileInView={"animate"}>
+        <Link
+          to={"/about"}
+          variants={fadeUp}
+          initial="initial"
+          viewport={{ once: true }}
+          whileInView={"animate"}
+          className="w-40 h-40 bg-shade2/15 hover:bg-shade1 hover:text-shade3 transition duration-300 rounded-full flex justify-center items-center ml-auto mr-8 mt-8 lg:ml-0 lg:mr-0 lg:mt-24"
+        >
+          <span className="text-xl font-medium">About Me</span>
+        </Link>
+      </motion.div>
     </div>
   );
 };
