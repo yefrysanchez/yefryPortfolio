5d575fed49c57fa006a64cd513f8951076328bf7
Yefry Sanchez <yefrymsp@gmail.com>
2025-10-02 23:27:37 -0400

    changed certs order

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -73,13 +73,14 @@ export default AboutPage;
 
 const certs = [
   {
-    title: "Meta Front-End Developer Specialization",
-    url: "https://www.coursera.org/account/accomplishments/specialization/ROUJXCAW6O6Q",
-  },
-  {
     title: "Google Cloud: Associate Cloud Engineer",
     url: "https://www.credly.com/badges/7ac77b31-e544-4863-9845-c8f4c93f8b40/linked_in_profile",
   },
+  {
+    title: "Meta Front-End Developer Specialization",
+    url: "https://www.coursera.org/account/accomplishments/specialization/ROUJXCAW6O6Q",
+  },
+
   {
     title: "AWS Certified Cloud Practitioner",
     url: "https://www.credly.com/badges/b6074a5a-a750-4142-b64d-c6ad47098995/linked_in_profile",
